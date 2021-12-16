9ec0ee4e2fc779efa3aa3d3095ac2b4f44187159
makito <noguchi.4424@gmail.com>
2021-12-16 22:45:50 +0900

    ハンバーガーメニュー追加

---
+++ html/js/menu/burger.js
@@ -0,0 +1,7 @@
+$(function() {
+	$('.burger-btn').on('click', function () {
+    $('.burger-btn').toggleClass('close');
+    $('.nav-wrapper').toggleClass('slide-in');
+    $('body').toggleClass('noscroll');
+  });
+});
